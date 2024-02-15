85d2a4bf3bfc8c86413ed844d7a2c486fa75bcb8
BAN96 <anokarb1996@gmail.com>
2024-02-15 17:53:56 +0530

    pushing hompage and login page changes

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,7 +1,14 @@
 package com.qa.pages;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.qa.base.TestBase;
 
@@ -17,9 +24,38 @@ public class HomePage extends TestBase{
 	@FindBy(xpath="//a[@id='nava']") private WebElement logoName;
 	
 	public HomePage() {
-		
-		
+		PageFactory.initElements(driver, this);
 	}
 	
+	public LoginPopup clickLogin() {
+		loginLink.click();
+		return new LoginPopup();
+	}
+	
+	public void clickSignup() {
+		signupLink.click();
+	}
+	public void clickOnCart() {
+		cartLink.click();
+	}
+	public void clickonAboutUS() {
+		abouUsLink.click();
+	}
+	public void clickOnContact() {
+		contactLink.click();
+	}
+	public void clickonHome() {
+		HomeLink.click();
+	}
+	
+	public void getLogo() {
+		File actualImg=logo.getScreenshotAs(OutputType.FILE);
+		try {
+			org.openqa.selenium.io.FileHandler.copy(actualImg, new File(".\\src\\main\\java\\com\\qa\\images\\"+logoName.getText().replaceAll(" ", "_")+".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
